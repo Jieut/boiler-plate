719ab3a4b526eb51f8fd72ce50f94386b77688ce
Jieut <leaves1245@naver.com>
2022-05-17 11:34:44 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser');
+const { User } =require('./models/User');
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jieut:jieut@boilerplate.gkzfq.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!')
+  res.send('Hello World!!')
 })
+
+app.post('/register', (req, res) => {
+
+    // 클라이언트가 보낸 정보를 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    console.log(`user: ${user}`);
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
